c378c4463795e40436601f9076edc41f4cf94432
Jakob-Niklas See <git@nwex.de>
2020-04-14 06:04:50 +0200

    Updated build properties, added welcome message, fixed tablist

--- src/main/java/de/nwex/ignore/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/de/nwex/ignore/mixin/MixinClientPlayNetworkHandler.java
@@ -2,15 +2,18 @@ package de.nwex.ignore.mixin;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.CommandDispatcher;
+import de.nwex.ignore.Chat;
 import de.nwex.ignore.IgnorePlayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.packet.ChatMessageS2CPacket;
 import net.minecraft.client.network.packet.CommandTreeS2CPacket;
+import net.minecraft.client.network.packet.GameJoinS2CPacket;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.command.CommandSource;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -31,6 +34,8 @@ public class MixinClientPlayNetworkHandler
 
     @Shadow
     private MinecraftClient client;
+
+    private Boolean once = true;
 
     @Inject(method = "onChatMessage", at = @At("HEAD"), cancellable = true)
     public void onChatMessage(ChatMessageS2CPacket chatMessageS2CPacket, CallbackInfo ci)
@@ -54,6 +59,33 @@ public class MixinClientPlayNetworkHandler
 
         this.client.inGameHud.addChatMessage(chatMessageS2CPacket.getLocation(), chatMessageS2CPacket.getMessage());
         ci.cancel();
+    }
+
+    @Inject(method = "onGameJoin", at = @At("HEAD"))
+    public void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci)
+    {
+        if(once)
+        {
+            once = false;
+
+            new Thread(() ->
+            {
+                try
+                {
+                    Thread.sleep(1000);
+                }
+                catch(InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+
+                Chat.print(new LiteralText("Loaded"), new LiteralText("")
+                    .append(new LiteralText("Ignoring ").formatted(IgnorePlayer.BASE))
+                    .append(new LiteralText(String.valueOf(IgnorePlayer.ignored.size())).formatted(IgnorePlayer.HIGHLIGHT))
+                    .append(new LiteralText(" player(s)").formatted(IgnorePlayer.BASE))
+                );
+            }).start();
+        }
     }
 
     @SuppressWarnings("unchecked")
--- src/main/java/de/nwex/ignore/mixin/MixinPlayerListHud.java
+++ src/main/java/de/nwex/ignore/mixin/MixinPlayerListHud.java
@@ -8,7 +8,6 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -16,16 +15,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerListHud.class)
 public class MixinPlayerListHud
 {
-    @Shadow
-    private Text header;
-
-    private Text originalHeader;
-
     @Inject(method = "getPlayerName", at = @At("RETURN"), cancellable = true)
     public void getPlayerName(PlayerListEntry playerListEntry, CallbackInfoReturnable<Text> ci)
     {
         Text original = playerListEntry.getDisplayName() != null ? playerListEntry.getDisplayName() : Team.modifyText(playerListEntry.getScoreboardTeam(), new LiteralText(playerListEntry.getProfile().getName()));
 
-        ci.setReturnValue(original.append(new LiteralText(IgnorePlayer.ignored.contains(playerListEntry.getProfile().getName()) ? " (ignored)" : "").formatted(Formatting.GRAY).formatted(Formatting.ITALIC)));
+        Text add = new LiteralText("");
+
+        if(IgnorePlayer.ignored.contains(playerListEntry.getProfile().getName()))
+        {
+            add.append(" (ignored)").formatted(Formatting.GRAY).formatted(Formatting.ITALIC);
+        }
+
+        ci.setReturnValue(new LiteralText("").append(original).append(add));
     }
 }
